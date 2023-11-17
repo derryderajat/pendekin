f5ee7be8d8b945d5a3b234a5f797107b5e9d45c1
derryderajat <derryd.derajat@gmail.com>
2023-11-17 16:18:33 +0700

    comment code generate qr when creating link

--- src/link/link.service.js
+++ src/link/link.service.js
@@ -3,13 +3,13 @@ const { createUrl, getOriginalLink } = require("./link.repository");
 
 const pendekin = async (user_id, original, short, protocol, host) => {
   const data = original;
-  const fileName = Date.now();
-  const qr = await generateQRWithLogo(data, fileName, protocol, host);
-  const { url } = qr;
+  // const fileName = Date.now();
+  // const qr = await generateQRWithLogo(data, fileName, protocol, host);
+  // const { url } = qr;
   const shortLink = `${protocol}://${host}/s/${short}`;
   let newLink;
   try {
-    newLink = await createUrl(user_id, original, shortLink, url);
+    newLink = await createUrl(user_id, original, shortLink, "");
   } catch (error) {
     throw new Error("short already exists");
   }
